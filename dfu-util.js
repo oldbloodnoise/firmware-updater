24b8672f8d5c8b67ee6c756994be07f53eef3e1b
Old Blood Noise Endeavors <oldbloodnoise@gmail.com>
2026-04-21 17:12:46 +0000

    didn't update newestFirmware

--- dfu-util.js
+++ dfu-util.js
@@ -276,14 +276,73 @@ var firmwareFile = null;
 
         let manifestationTolerant = true;
 
+        let newest = {
+            BlackFountain: "4.0G",
+            DarkStar: "3.0R",
+            Sunlight: "2.4H",
+            Bathing: "1.0G",
+            Parting: "1.0C",
+            Setback: "1.0B"
+        }
+
         let fwBaseUrl = "/firmware/"
         let newestFirmware = {
-            DarkStar: `${fwBaseUrl}DarkStar-3-0Q-firmware.bin`,
-            BlackFountain: `${fwBaseUrl}BlackFountain-4-0F-firmware.bin`,
-            Bathing: `${fwBaseUrl}Bathing-1-0F-firmware.bin`,
-            Sunlight: `${fwBaseUrl}Sunlight-2-4G-firmware.bin`,
-            Parting: `${fwBaseUrl}Parting-1-0B-firmware.bin`,
+            DarkStar: `${fwBaseUrl}DarkStar-3-0R-firmware.bin`,
+            BlackFountain: `${fwBaseUrl}BlackFountain-4-0G-firmware.bin`,
+            Bathing: `${fwBaseUrl}Bathing-1-0G-firmware.bin`,
+            Sunlight: `${fwBaseUrl}Sunlight-2-4H-firmware.bin`,
+            Parting: `${fwBaseUrl}Parting-1-0C-firmware.bin`,
+            Setback: `${fwBaseUrl}Setback-1-0B-firmware.bin`,
         }
+
+        let patchNotes = {
+            DarkStar:
+            {
+                "3.0J": "Initial production version",
+                "3.0K": "Fixed pot override issue on initial preset recall",
+                "3.0L": "Improved processing speed and memory storage routines",
+                "3.0M": "Fixed issue where unity pitch would not be recalled correctly from preset",
+                "3.0Q": "Fixed new pitch recall issues from 3.0M, improved factory reset",
+                "3.0R": "Fixes issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device"
+            },
+            BlackFountain:
+            {
+                "1.0B": "Initial production version",
+                "1.0C": "Fixed issue with MIDI channel recall on startup",
+                "1.0D": "Fixed conflict with knob settings when recalling presets",
+                "1.0E": "Added override for scooch-as-tap-tempo when assigning new expression setting",
+                "1.0F": "Fixed issue from 1.0E where some bug fixes were reverted back",
+                "1.0G": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
+            },
+            Bathing:
+            {
+                "1.0D": "Initial production version",
+                "1.0E": "Improved taper of wet and dry knobs",
+                "1.0F": "Fixed issue with noise occurring on right output when stages set to 5 and rate above zero",
+                "1.0G": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
+            },
+            Sunlight:
+            {
+                "2.4B": "Early demo units only",
+                "2.4C": "Initial production version",
+                "2.4D": "When holding trails with Aux, allowed movement of Decay knob to override the hold",
+                "2.4E": "Fixed conflict between expression control of Decay knob and using Aux to hold trails",
+                "2.4F": "Fixed issue with subdivision recall for subdivisions faster than x1\nFixed issue where dynamically held trails would repeat indefinitely after turning pedal off in trails mode",
+                "2.4G": "Fixed rate recall issue when preset was stored after changing rate knob",
+                "2.4H": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
+            },
+            Parting:
+            {
+                "1.0B": "Initial production version",
+                "1.0C": "Fixes issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device\nFixed issue with Rate storing incorrectly in preset when rate subdivisions aren’t x1",
+            },
+            Setback:
+            {
+                "1.0A": "First production version",
+                "1.0B": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
+            }
+        };
+
 
         function onDisconnect(reason) {
             if (reason) {
@@ -425,15 +484,6 @@ var firmwareFile = null;
             let Parting = "Parting";
             let Setback = "Setback";
 
-            let newest = {
-                BlackFountain: "4.0G",
-                DarkStar: "3.0R",
-                Sunlight: "2.4H",
-                Bathing: "1.0G",
-                Parting: "1.0C",
-                Setback: "1.0B"
-            }
-
             let pedalIdentifier = {
                 "008362291841819763": [BlackFountain, "4.0B"],
                 "00836331851824563": [BlackFountain, "4.0C"],
@@ -463,54 +513,6 @@ var firmwareFile = null;
                 "008361691220845120": [Parting, "1.0C"],
                 "008362051901814570": [Setback, "1.0A"],
                 "008362451901819370": [Setback, "1.0B"],
-            };
-
-            let patchNotes = {
-                DarkStar: 
-                {
-                    "3.0J": "Initial production version",
-                    "3.0K": "Fixed pot override issue on initial preset recall",
-                    "3.0L": "Improved processing speed and memory storage routines",
-                    "3.0M": "Fixed issue where unity pitch would not be recalled correctly from preset",
-                    "3.0Q": "Fixed new pitch recall issues from 3.0M, improved factory reset",
-                    "3.0R": "Fixes issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device"
-                },
-                BlackFountain: 
-                {
-                    "1.0B": "Initial production version",
-                    "1.0C": "Fixed issue with MIDI channel recall on startup",
-                    "1.0D": "Fixed conflict with knob settings when recalling presets",
-                    "1.0E": "Added override for scooch-as-tap-tempo when assigning new expression setting",
-                    "1.0F": "Fixed issue from 1.0E where some bug fixes were reverted back",
-                    "1.0G": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
-                },
-                Bathing:
-                {
-                    "1.0D": "Initial production version",
-                    "1.0E": "Improved taper of wet and dry knobs",
-                    "1.0F": "Fixed issue with noise occurring on right output when stages set to 5 and rate above zero",
-                    "1.0G": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
-                },
-                Sunlight:
-                {
-                    "2.4B": "Early demo units only",
-                    "2.4C": "Initial production version",
-                    "2.4D": "When holding trails with Aux, allowed movement of Decay knob to override the hold",
-                    "2.4E": "Fixed conflict between expression control of Decay knob and using Aux to hold trails",
-                    "2.4F": "Fixed issue with subdivision recall for subdivisions faster than x1\nFixed issue where dynamically held trails would repeat indefinitely after turning pedal off in trails mode",
-                    "2.4G": "Fixed rate recall issue when preset was stored after changing rate knob",
-                    "2.4H": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
-                },
-                Parting:
-                {
-                    "1.0B": "Initial production version",
-                    "1.0C": "Fixes issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device\nFixed issue with Rate storing incorrectly in preset when rate subdivisions aren’t x1",
-                },
-                Setback:
-                {
-                    "1.0A": "First production version",
-                    "1.0B": "Fixed issue where restart of external MIDI controlling device would halt MIDI communication until restart of OBNE device",
-                }
             };
 
             if (!device || !device.device_.opened) {
@@ -559,7 +561,7 @@ var firmwareFile = null;
                             logInfo(patchNotes[pedalName][newest[pedalName]]);
                             readServerFirmwareFile(newestFirmware[pedalName]).then((buffer) => {
                                 firmwareFile = buffer;
-                            });     
+                            });
                             downloadButton.classList.remove("hidden");
                         }
                     }
